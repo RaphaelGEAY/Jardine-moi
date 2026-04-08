b1e23d8bd51423dde8f9687dbcaa93c45cce574f
geraphael <raphael.geay@ynov.com>
2026-04-08 13:53:47 +0200

    merge complete

--- app/src/main/java/com/example/jardinemoi/SupabaseManager.kt
+++ app/src/main/java/com/example/jardinemoi/SupabaseManager.kt
@@ -1,9 +1,10 @@
 package com.example.jardinemoi
 
 import io.github.jan.supabase.SupabaseClient
-import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.auth.Auth
+import io.github.jan.supabase.createSupabaseClient
 
+@Suppress("SpellCheckingInspection")
 object SupabaseManager {
 
     lateinit var client: SupabaseClient
@@ -11,7 +12,7 @@ object SupabaseManager {
     fun init() {
         client = createSupabaseClient(
             supabaseUrl = "https://mhvqouoferviewbgsxka.supabase.co",
-            supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1odnFvdW9mZXJ2aWV3YmdzeGthIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2MjYxNjUsImV4cCI6MjA5MTIwMjE2NX0.Z6JrMTwvNRx15fWVvmMuNeif_RZV3FE6O3N5Akudfgo" // ✔️ ANON KEY
+            supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1odnFvdW9mZXJ2aWV3YmdzeGthIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2MjYxNjUsImV4cCI6MjA5MTIwMjE2NX0.Z6JrMTwvNRx15fWVvmMuNeif_RZV3FE6O3N5Akudfgo"
         ) {
             install(Auth)
         }
